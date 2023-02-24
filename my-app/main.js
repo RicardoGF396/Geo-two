aca868cd61eb9aab364fdb8e7284b132111e4062
Ricardo <ricardogf396@gmail.com>
2023-02-23 19:06:42 -0600

    First commit

---
+++ my-app/main.js
@@ -0,0 +1,17 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [-11322453.496888205,2410093.566001577],
+    zoom: 18
+  })
+});
